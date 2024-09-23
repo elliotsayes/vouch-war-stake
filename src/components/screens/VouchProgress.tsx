ad0a948f7cf0b205ad9132c5de3589f7d635bb55
Elliot Sayes <elliot@sayes.cloud>
2024-09-23 20:25:41 +0800

    UI for staking

--- src/components/screens/VouchProgress.tsx
+++ src/components/screens/VouchProgress.tsx
@@ -6,6 +6,23 @@ import { VouchBreakdown } from "../VouchBreakdown";
 import { useQuery } from "@tanstack/react-query";
 import { VouchButtons } from "../VouchButtons";
 import { useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 
 export interface VouchProgressProps {
   targetValue: VouchValue;
@@ -16,31 +33,72 @@ export const VouchProgress = ({
   targetValue,
   profileId,
 }: VouchProgressProps) => {
-  const walletId = useActiveAddress();
-  const [showModal, setShowModal] = useState(false);
+  const [showStakeSheet, setShowStakeSheet] = useState(false);
+  const [showConfirmQuitDialog, setShowConfirmQuitDialog] = useState(false);
 
   return (
-    // Full Screen
-    <div className="flex flex-col h-screen relative">
-      <div className="absolute top-0 right-0 p-2">
-        <ConnectButton />
-      </div>
-      {/* Centered box */}
-      <div className="flex flex-col items-center justify-center h-full text-center gap-4">
-        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold pb-4">
-          Get Vouched <br />
-          on the Permaweb
-        </h1>
-        <div className="pb-4">
-          <GoalProgress targetValue={targetValue} profileId={profileId} />
+    <Sheet open={showStakeSheet} onOpenChange={setShowStakeSheet}>
+      <AlertDialog
+        open={showConfirmQuitDialog}
+        onOpenChange={setShowConfirmQuitDialog}
+      >
+        <div className="flex flex-col h-screen relative">
+          <div className="absolute top-0 right-0 p-2">
+            <ConnectButton />
+          </div>
+          {/* Centered box */}
+          <div className="flex flex-col items-center justify-center h-full text-center gap-4">
+            <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold pb-4">
+              Get Vouched <br />
+              on the Permaweb
+            </h1>
+            <div className="pb-4">
+              <GoalProgress targetValue={targetValue} profileId={profileId} />
+            </div>
+            <div>
+              <h1 className="text-lg mb-2 text-muted-foreground">
+                Increase your vouch score with these services
+              </h1>
+              <VouchButtons
+                onActionVoucherClick={() => setShowStakeSheet(true)}
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <h1 className="text-lg mb-2 text-muted-foreground">
-            Increase your vouch score with these services
-          </h1>
-          <VouchButtons onActionVoucherClick={() => setShowModal(true)} />
-        </div>
-      </div>
-    </div>
+        <SheetContent
+          side={"bottom"}
+          onInteractOutside={(e) => {
+            e.preventDefault();
+            setShowConfirmQuitDialog(true);
+          }}
+          onEscapeKeyDown={(e) => {
+            e.preventDefault();
+            setShowConfirmQuitDialog(true);
+          }}
+        >
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Cancel Staking?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Form data will be cleared.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={() => setShowStakeSheet(false)}>
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </Sheet>
   );
 };
