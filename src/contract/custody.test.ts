f5475d25f0a09e7f23c1b042277e5ea75b7217ec
Elliot Sayes <elliot@sayes.cloud>
2024-09-23 13:59:08 +0800

    Basic tests

---
+++ src/contract/custody.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "vitest";
+import { getInfoQuery, depositMutation, getActiveStakesQuery } from "./custody";
+import { queryClient } from "../lib/query";
+
+const testContractId = "48Bg_s9zmZzLKlvpeXPs15qID2uL1xNzJ89sPz5VPhM";
+
+describe("Custody contract", () => {
+  it("getInfoQuery", async () => {
+    const res = await queryClient.fetchQuery(getInfoQuery(testContractId));
+    expect(res).toMatchSnapshot();
+  });
+
+  it("getActiveStakesQuery", async () => {
+    const res = await queryClient.fetchQuery(
+      getActiveStakesQuery(testContractId)
+    );
+    expect(res).toMatchSnapshot();
+  });
+});
---
+++ src/contract/custody.ts
@@ -0,0 +1,92 @@
+import { dryrun, message } from "@permaweb/aoconnect";
+import { MutationOptions, queryOptions } from "@tanstack/react-query";
+import { getTagValue } from "../lib/arweave";
+import { AoSigner } from "../hooks/useAoSigner";
+
+const WAR_TOKEN_PROCESS_ID = import.meta.env.VITE_WAR_TOKEN_PROCESS_ID!;
+
+export const getInfoQuery = (processId: string) =>
+  queryOptions({
+    queryKey: ["Custody", processId, "Info"],
+    queryFn: async () => {
+      const res = await dryrun({
+        process: processId,
+        tags: [
+          {
+            name: "Action",
+            value: "Info",
+          },
+        ],
+      });
+      const error = getTagValue(res.Messages[0].Tags, "Error");
+      if (error) {
+        return error;
+      }
+      return JSON.parse(res.Messages[0].Data);
+    },
+  });
+
+export const getActiveStakesQuery = (processId: string, walletId?: string) =>
+  queryOptions({
+    queryKey: ["Custody", processId, "Get-Active-Stakes"],
+    queryFn: async () => {
+      const res = await dryrun({
+        process: processId,
+        tags: [
+          {
+            name: "Action",
+            value: "Custody.Get-Active-Stakes",
+          },
+          ...(walletId
+            ? [
+                {
+                  name: "Wallet-Id",
+                  value: walletId,
+                },
+              ]
+            : []),
+        ],
+      });
+      const error = getTagValue(res.Messages[0].Tags, "Error");
+      if (error) {
+        return error;
+      }
+      return JSON.parse(res.Messages[0].Data);
+    },
+  });
+
+export const depositMutation = (
+  custodyProcessId: string,
+  tokenId: string,
+  quantity: string,
+  stakeDurationMs: number,
+  aoSigner: AoSigner
+): MutationOptions => ({
+  mutationKey: ["Custody", custodyProcessId, "Deposit", tokenId],
+  mutationFn: async () => {
+    const messageId = await message({
+      process: tokenId,
+      tags: [
+        {
+          name: "Action",
+          value: "Transfer",
+        },
+        {
+          name: "Recipient",
+          value: custodyProcessId,
+        },
+        {
+          name: "Quantity",
+          value: quantity,
+        },
+        {
+          name: "X-Stake-Duration",
+          value: stakeDurationMs.toString(),
+        },
+      ],
+      signer: aoSigner,
+    });
+    console.log({ messageId });
+    return messageId;
+  },
+});
