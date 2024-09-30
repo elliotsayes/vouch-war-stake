215c91a7ba3a98cca8e412fe3bdc3cc433429541
Elliot Sayes <elliot@sayes.cloud>
2024-10-01 03:08:58 +0800

    Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { ArweaveWalletKit } from "arweave-wallet-kit";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/query";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createRouter,
+  createHashHistory,
+} from "@tanstack/react-router";
 import "./index.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
-// Create a new router instance
-const router = createRouter({ routeTree });
+// Create a new history & router instance
+const hashHistory = createHashHistory();
+const router = createRouter({ routeTree, history: hashHistory });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
