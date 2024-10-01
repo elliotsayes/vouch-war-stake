b78b95dd005608aa188f3b311518fa3b6c704747
Elliot Sayes <elliot@sayes.cloud>
2024-10-01 18:46:21 +0800

    Back button

--- src/components/screens/VouchProgress.tsx
+++ src/components/screens/VouchProgress.tsx
@@ -31,6 +31,15 @@ import { StakeConfiguration } from "../StakeConfiguration";
 import { useWhitelistedVouchData } from "@/hooks/useVouchHistory";
 import { DepositParameters } from "@/contract/custody";
 import { ConnectWalletBlocker } from "./ConnectWalletBlocker";
+import { ArrowLeftIcon } from "lucide-react";
+import { Link } from "@tanstack/react-router";
+import { buttonVariants } from "../ui/button";
+import {
+  TooltipProvider,
+  TooltipTrigger,
+  Tooltip,
+  TooltipContent,
+} from "../ui/tooltip";
 
 export interface VouchProgressProps {
   targetValue: VouchValue;
@@ -73,91 +82,109 @@ export const VouchProgress = ({
   );
 
   return (
-    <Sheet open={showStakeSheet} onOpenChange={setShowStakeSheet}>
-      <AlertDialog
-        open={showConfirmSubmitDialog}
-        onOpenChange={setShowConfirmSubmitDialog}
-      >
-        <div className="flex flex-col h-screen relative">
-          <div className="absolute top-0 right-0 p-2">
-            <ConnectButton />
-          </div>
-          {/* Centered box */}
-          <div className="flex flex-col items-center justify-center h-full text-center gap-4">
-            <h1 className="text-5xl md:text-6xl font-bold pb-4">
-              Get Vouched <br />
-              on the Permaweb
-            </h1>
-            <div className="pb-4">
-              <GoalProgress
-                targetValue={targetValue}
-                {...(showStakeSheet
-                  ? {
-                      bonusValue,
-                      projectedMeetsTarget,
-                    }
-                  : {})}
-                profileId={profileId}
-              />
+    <TooltipProvider>
+      <Sheet open={showStakeSheet} onOpenChange={setShowStakeSheet}>
+        <AlertDialog
+          open={showConfirmSubmitDialog}
+          onOpenChange={setShowConfirmSubmitDialog}
+        >
+          <div className="flex flex-col h-screen relative">
+            <div className="absolute top-0 left-0 p-2">
+              <Tooltip>
+                <TooltipTrigger>
+                  <Link
+                    className={buttonVariants({
+                      variant: "outline",
+                      size: "icon",
+                    })}
+                    to={"/"}
+                  >
+                    <ArrowLeftIcon />
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent align="start">Back to home</TooltipContent>
+              </Tooltip>
             </div>
-            <div
-              className={`transition-opacity duration-500 ${showStakeSheet ? "opacity-50" : ""}`}
-            >
-              <h1 className="text-lg mb-4 text-muted-foreground">
-                Increase your vouch score with these services
+            <div className="absolute top-0 right-0 p-2">
+              <ConnectButton />
+            </div>
+            {/* Centered box */}
+            <div className="flex flex-col items-center justify-center h-full text-center gap-4">
+              <h1 className="text-5xl md:text-6xl font-bold pb-4">
+                Get Vouched <br />
+                on the Permaweb
               </h1>
-              <VouchButtons
-                onActionVoucherClick={() => {
-                  if (connected) {
-                    setShowStakeSheet(true);
-                  } else {
-                    connect();
-                  }
-                }}
+              <div className="pb-4">
+                <GoalProgress
+                  targetValue={targetValue}
+                  {...(showStakeSheet
+                    ? {
+                        bonusValue,
+                        projectedMeetsTarget,
+                      }
+                    : {})}
+                  profileId={profileId}
+                />
+              </div>
+              <div
+                className={`transition-opacity duration-500 ${showStakeSheet ? "opacity-50" : ""}`}
+              >
+                <h1 className="text-lg mb-4 text-muted-foreground">
+                  Increase your vouch score with these services
+                </h1>
+                <VouchButtons
+                  onActionVoucherClick={() => {
+                    if (connected) {
+                      setShowStakeSheet(true);
+                    } else {
+                      connect();
+                    }
+                  }}
+                />
+              </div>
+              <div
+                className={`transition-all duration-500 ${showStakeSheet ? "h-1/4" : "h-0"}`}
               />
             </div>
-            <div
-              className={`transition-all duration-500 ${showStakeSheet ? "h-1/4" : "h-0"}`}
-            />
           </div>
-        </div>
-        <SheetContent side={"bottom"}>
-          <SheetHeader className="md:w-[80%] max-w-md mx-auto">
-            <SheetTitle className="text-center">
-              Earn vouch points by staking wrapped Arweave
-            </SheetTitle>
-          </SheetHeader>
-          <ConnectWalletBlocker>
-            {() => (
-              <StakeConfiguration
-                targetValue={targetValue}
-                bonusValue={bonusValue}
-                setBonusValue={setBonusValue}
-                projectedMeetsTarget={projectedMeetsTarget}
-                onSubmitDeposit={onSubmitDeposit}
-              />
-            )}
-          </ConnectWalletBlocker>
-        </SheetContent>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Confirm Deposit?</AlertDialogTitle>
-            <AlertDialogDescription>
-              Deposit quantity is insufficient for the goal. Deposit anyway?
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction
-              onClick={() => {
-                onConfirmDeposit(depositParameters!);
-              }}
-            >
-              Continue
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </Sheet>
+          <SheetContent side={"bottom"}>
+            <SheetHeader className="md:w-[80%] max-w-md mx-auto">
+              <SheetTitle className="text-center">
+                Earn vouch points by staking wrapped Arweave
+              </SheetTitle>
+            </SheetHeader>
+            <ConnectWalletBlocker>
+              {() => (
+                <StakeConfiguration
+                  targetValue={targetValue}
+                  bonusValue={bonusValue}
+                  setBonusValue={setBonusValue}
+                  projectedMeetsTarget={projectedMeetsTarget}
+                  onSubmitDeposit={onSubmitDeposit}
+                />
+              )}
+            </ConnectWalletBlocker>
+          </SheetContent>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Confirm Deposit?</AlertDialogTitle>
+              <AlertDialogDescription>
+                Deposit quantity is insufficient for the goal. Deposit anyway?
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => {
+                  onConfirmDeposit(depositParameters!);
+                }}
+              >
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </Sheet>
+    </TooltipProvider>
   );
 };
