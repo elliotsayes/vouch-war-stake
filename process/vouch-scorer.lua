32486ba8e17df9b939cf01aa0a86fe96572ab08c
Elliot Sayes <elliot@sayes.cloud>
2024-10-16 19:57:02 +0800

    Update scoring logic

--- process/vouch-scorer.lua
+++ process/vouch-scorer.lua
@@ -1,8 +1,12 @@
 VOUCH_PROCESS = "ZTTO02BL2P-lseTLUgiIPD9d0CF1sc4LbMA2AQ7e9jo"
 VOUCHER_WHITELIST = {
+  -- Vouch-X
   ["Ax_uXyLQBPZSQ15movzv9-O1mDo30khslqN64qD27Z8"] = true,
+  -- Vouch-Gitcoin-Passport
   ["k6p1MtqYhQQOuTSfN8gH7sQ78zlHavt8dCDL88btn9s"] = true,
+  -- Vouch-AO-Balance
   ["QeXDjjxcui7W2xU08zOlnFwBlbiID4sACpi0tSS3VgY"] = true,
+  -- Vouch-wAR-Stake
   ["3y0YE11i21hpP8UY0Z1AVhtPoJD4V_AbEBx-g0j9wRc"] = true,
 }
 
@@ -20,28 +24,36 @@ function GetVouchScoreUsd(walletId)
   local resp = Handlers.receive({
     From = VOUCH_PROCESS,
     Action = "VouchDAO.Vouches",
+    ID = walletId,
   })
 
   local success, data = pcall(json.decode, resp.Data)
-  if not success or type(data) ~= 'table' or data['Vouchers'] == nil then
+  if not success or type(data) ~= 'table' then
     print("Invalid data: " .. resp.Data)
     return 0
   end
 
-  if data['Vouches-For'] ~= walletId then
-    print("Vouches-For mismatch, expected: " .. walletId .. ", got: " .. tostring(data['Vouches-For']))
+  local vouches = data['Vouchers']
+  if vouches == nil then
+    print("No Vouchers")
     return 0
   end
 
-  local vouches = data['Vouchers']
   local score = 0
-
   for voucher, vouch in pairs(vouches) do
     if VOUCHER_WHITELIST[voucher] then
-      -- 1.34-USD -> 1.34
-      local valueStr = string.match(vouch.Value, "([%d%.]+)-USD")
-      if valueStr ~= nil then
-        score = score + tonumber(valueStr)
+      local vouchFor = vouch['Vouch-For']
+      if vouchFor ~= walletId then
+        print(voucher .. " has Vouch-For mismatch, expected: " .. walletId .. ", got: " .. vouchFor)
+      else
+        -- 1.34-USD -> 1.34
+        local valueStr = string.match(vouch.Value, "([%d%.]+)-USD")
+        local value = tonumber(valueStr)
+        if valueStr == nil or value == nil then
+          print(voucher .. " has invalid value: " .. vouch.Value)
+        else
+          score = score + value
+        end
       end
     end
   end
