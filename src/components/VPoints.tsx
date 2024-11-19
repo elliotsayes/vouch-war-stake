7fc5f389b12840b0806d5c1ca0922b00915d709a
Elliot Sayes <elliot@sayes.cloud>
2024-11-19 20:59:37 +0800

    Tweak VP

--- src/components/VPoints.tsx
+++ src/components/VPoints.tsx
@@ -35,7 +35,7 @@ export const VPoints = ({ noTooltip }: VPointsProps) => {
 };
 
 const VPointsBadge = () => (
-  <div className="bg-primary/20 text-primary-foreground text-sm flex flex-row items-center pt-[3px] pb-1 px-1.5 rounded-md drop-shadow-md shadow-black">
+  <div className="bg-primary/25 text-primary-foreground text-sm flex flex-row items-center pt-[3px] pb-1 px-1.5 rounded-md drop-shadow-md shadow-black">
     <img
       src="/images/vouch_dao-v2.svg"
       className="w-4 h-4 mr-1 drop-shadow-sm"
